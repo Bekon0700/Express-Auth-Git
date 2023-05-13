24c16e525beae639d059a3264488cc8a5e2e1651
Emdadul <emdadul5023@onnorokom.com>
2023-05-13 17:36:41 +0600

    prisma added to login

--- controller/authContoller.js
+++ controller/authContoller.js
@@ -17,8 +17,6 @@ exports.registration = async (req, res) => {
 
     const hashPass = await bcrypt.hash(password, 12);
 
-    console.log(name, phone, password, hashPass)
-
     try{
         const user = await prisma.user.create({
             data: {
@@ -49,27 +47,45 @@ const createToken = (user, status, res) => {
     })
 }
 
-exports.login = (req, res) => {
+exports.login = async (req, res) => {
     const {phone, password} = req.body
 
-    dbConnectionPool.query(`SELECT * FROM users WHERE phone='${phone}'`, async (err, result) => {
-        if(err){
-            console.log(err)
-            return
-        }
+    // dbConnectionPool.query(`SELECT * FROM users WHERE phone='${phone}'`, async (err, result) => {
+    //     if(err){
+    //         console.log(err)
+    //         return
+    //     }
 
-        if(result.length == 0) {
-            return errorProvider(res, 'No User found', 404);
-        }
+    //     if(result.length == 0) {
+    //         return errorProvider(res, 'No User found', 404);
+    //     }
 
-        const user = result;
-        const compare = await bcrypt.compare(password, user[0].password)
+    //     const user = result;
+    //     const compare = await bcrypt.compare(password, user[0].password)
         
-        if(!compare) {
-            return errorProvider(res, 'Password error', 403);
-        }
+    //     if(!compare) {
+    //         return errorProvider(res, 'Password error', 403);
+    //     }
 
-        createToken(user[0], 201, res)
-    })
+    //     createToken(user[0], 201, res)
+    // })
+    console.log(phone)
+    try{
+        const user = await prisma.user.findFirstOrThrow({
+            where: {
+                phone
+            }
+        })
+        console.log(user)
+        res.status(200).json({
+            message: 'Login Done.',
+            // user
+        })
+    }catch (err) {
+        console.log(err.message)
+        res.status(400).json({
+            message: err.message,
+        })
+    }
 
 }
